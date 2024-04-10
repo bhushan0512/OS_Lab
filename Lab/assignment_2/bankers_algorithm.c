acaa07c22930a911708fa8c41eaf0ab748a4f324
bhushan0512 <bhushanabangera687@gmail.com>
2024-04-10 09:31:30 +0530

    bankers algorithm

---
+++ Lab/assignment_2/bankers_algorithm.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+
+void main()
+{
+    int n, m, i, j, k, alloc[5][3], max[5][3], avail[3];
+
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    printf("Enter the number of resources: ");
+    scanf("%d", &m);
+
+    printf("Enter the values for allocation:\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            scanf("%d", &alloc[i][j]);
+        }
+    }
+
+    printf("Enter the values for max:\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            scanf("%d", &max[i][j]);
+        }
+    }
+
+    printf("Enter the values for available:\n");
+    for (j = 0; j < m; j++)
+    {
+        scanf("%d", &avail[j]);
+    }
+
+    int f[n], ans[n], ind = 0;
+    for (k = 0; k < n; k++)
+    {
+        f[k] = 0;
+    }
+
+    int need[n][m];
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+            need[i][j] = max[i][j] - alloc[i][j];
+    }
+
+    int y = 0;
+    for (k = 0; k < n; k++)
+    {
+        for (i = 0; i < n; i++)
+        {
+
+            if (f[i] == 0)
+            {
+                int flag = 0;
+
+                for (j = 0; j < m; j++)
+                {
+                    if (need[i][j] > avail[j])
+                    {
+                        flag = 1;
+                        break;
+                    }
+                }
+
+                if (flag == 0)
+                {
+                    ans[ind++] = i;
+
+                    for (y = 0; y < m; y++)
+                        avail[y] += alloc[i][y];
+                    f[i] = 1;
+                }
+            }
+        }
+    }
+
+    int flag = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if (f[i] == 0)
+        {
+            flag = 0;
+            printf("The following system is not safe");
+            break;
+        }
+    }
+
+    if (flag == 1)
+    {
+        printf("Following is the SAFE Sequence\n");
+        for (i = 0; i < n - 1; i++)
+            printf(" P%d ->", ans[i]);
+        printf(" P%d", ans[n - 1]);
+    }
+}
